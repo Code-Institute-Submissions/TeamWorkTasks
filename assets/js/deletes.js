898fd98b54b471b170200ccbe85346a25507fb04
Dave O'Dea <hi@daveodea.com>
2019-02-06 17:46:45 +0000

    Add delete apir proxy

--- assets/js/deletes.js
+++ assets/js/deletes.js
@@ -1,14 +1,12 @@
 function deleteTask(task_id) {
-    console.log("Task DELETED: " + task_id);
-    
+
 
     axios({
             method: 'DELETE',
-            url: 'https://' + SiteName + '.teamwork.com/tasks/' + task_id + '.json',
-            auth: {
-                username: APIKey,
-                password: ':xxx'
-            },
+            url: 'https://hi-21ca23a1-eval-prod.apigee.net/singletask/' + task_id + '.json',
+            headers: {
+                'Authorization': "Bearer " + APIKey
+            }
         })
         .then(function (response) {
             setTimeout(() => {
@@ -16,6 +14,7 @@ function deleteTask(task_id) {
                 getAllTasksCompleted();
             }, 100);
             console.log(response.statusText);
+            console.log("Task DELETED: " + task_id);
         })
         .catch(function (error) {
             console.log(error.statusText);
